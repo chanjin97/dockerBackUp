3134a91ef0caa7f963cfd2d8a6b49a6ee2591f12
chanjin97 <chanjindark@naver.com>
2024-09-06 19:39:44 +0900

    first BackUp

---
+++ 15_shop/src/main/java/com/study/spring/config/CustomSecurityConfig.java
@@ -0,0 +1,81 @@
+package com.study.spring.config;
+
+import java.util.Arrays;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.study.spring.security.filter.JWTCheckFilter;
+import com.study.spring.security.handler.APILoginFailHandler;
+import com.study.spring.security.handler.APILoginSuccessHandler;
+import com.study.spring.security.handler.CustomAccessDeniedHandler;
+
+
+@Configuration
+@Log4j2
+@ReadingConverter
+@EnableMethodSecurity
+public class CustomSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		log.info("---------------- #1 SecurityFilterChain -------------------");
+		
+		http.cors(httpSecurityCorsConfigurer -> {
+			httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource());
+		});
+		
+		//session 제거
+        http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		
+		http.csrf(config -> config.disable());
+		
+		http.formLogin(
+				config -> {
+					config.loginPage("/api/member/login"); //username:user1@aaa.com / password:1111
+					config.successHandler(new APILoginSuccessHandler());
+					config.failureHandler(new APILoginFailHandler());
+				});
+		
+		http.addFilterBefore(new JWTCheckFilter(), UsernamePasswordAuthenticationFilter.class);
+		
+		http.exceptionHandling(config -> {
+			config.accessDeniedHandler(new CustomAccessDeniedHandler());
+		});
+
+		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD","GET","POST","PUT","DELETE","OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization","Cache-Control","Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+
+    }
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ 15_shop/src/main/java/com/study/spring/controller/ProductController.java
@@ -0,0 +1,139 @@
+package com.study.spring.controller;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.study.spring.dto.PageRequestDTO;
+import com.study.spring.dto.PageResponseDTO;
+import com.study.spring.dto.ProductDTO;
+import com.study.spring.service.ProductService;
+import com.study.spring.util.CustomFileUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/api/products")
+public class ProductController {
+
+	private final ProductService productService;
+	
+	private final CustomFileUtil fileUtil;
+	
+	@PostMapping("/")
+	public Map<String,Long> register(ProductDTO productDTO) {
+		log.info("register:" + productDTO);
+		
+		List<MultipartFile> files = productDTO.getFiles();
+		List<String> uploadFileNames = fileUtil.saveFiles(files);
+		
+		productDTO.setUploadFileNames(uploadFileNames);
+		
+		log.info(uploadFileNames);
+		
+		Long pno = productService.register(productDTO);
+		
+		return Map.of("Result",pno);
+	}
+	
+	@PreAuthorize("hasAnyRole('ROLE_MEMBER','ROLE_ADMIN')")
+	@GetMapping("/view/{fileName}")
+	public ResponseEntity<Resource> viewFilGet(
+			@PathVariable("fileName") String fileName
+			){
+		
+		return fileUtil.getFile(fileName);
+	}
+	
+	
+	@PreAuthorize("hasAnyRole('ROLE_MEMBER','ROLE_ADMIN')")
+	@GetMapping("/{pno}")
+	public ProductDTO read(
+			@PathVariable("pno") Long pno
+			) {
+		return productService.get(pno);
+	}
+	
+	
+	@GetMapping("/list")
+	public PageResponseDTO<ProductDTO> list(PageRequestDTO pageRequestDTO){
+		return productService.getList(pageRequestDTO);
+	}
+	
+	
+	@PutMapping("/{pno}")
+	public Map<String,String> modify(
+			@PathVariable("pno") Long pno,
+			ProductDTO productDTO
+			){
+		
+		productDTO.setPno(pno);
+		
+		//이전파일이름들
+		ProductDTO oldProductDTO = productService.get(pno);
+		List<String> oldFileNames = oldProductDTO.getUploadFileNames();
+		
+		//새로운파일이름들
+		List<MultipartFile> files = productDTO.getFiles();
+		List<String> currentUploadFileNames = fileUtil.saveFiles(files);
+		
+		//유지된파일들
+		List<String> uploadedFileNames = productDTO.getUploadFileNames();
+		
+		if(currentUploadFileNames != null && currentUploadFileNames.size()>0) {
+			uploadedFileNames.addAll(currentUploadFileNames);
+		}
+		
+		
+		//수정작업
+		productService.modify(productDTO);
+		
+		
+		if(oldFileNames != null && oldFileNames.size()>0) {
+			
+			List<String> removeFiles = oldFileNames.stream()
+					.filter(fileName -> uploadedFileNames.indexOf(fileName) == -1)
+					.collect(Collectors.toList());
+			
+			fileUtil.deleteFiles(removeFiles);
+		}
+		
+		
+		
+		
+		return Map.of("Result","success");
+	}
+	
+	@DeleteMapping("/{pno}")
+	public Map<String,String> remove(
+			@PathVariable("pno") Long pno
+			){
+		
+		List<String> oldFileNames = productService.get(pno).getUploadFileNames();
+		
+		productService.remove(pno);
+		
+		fileUtil.deleteFiles(oldFileNames);
+		return Map.of("Result","success");
+	}
+	
+	
+	
+	
+	
+}
